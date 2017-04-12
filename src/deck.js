7a33e10d9cbcd075e3175ff06a0f3b843116a68c
Weiming Zhai <wmzhai@gmail.com>
2017-04-12 17:54:03 +0800

    spring back to default

--- src/deck.js
+++ src/deck.js
@@ -18,7 +18,9 @@ class Deck extends Component {
       onPanResponderMove: (event, gesture) => {
         position.setValue({ x: gesture.dx , y: gesture.dy })
       },
-      onPanResponderRelease: () => {}
+      onPanResponderRelease: () => {
+        this.resetPosition();
+      }
     })
 
     this.state = { panResponder, position }
@@ -35,6 +37,12 @@ class Deck extends Component {
       ...position.getLayout(),
       transform: [{rotate}]
     }
+  }
+
+  resetPosition() {
+    Animated.spring(this.state.position,{
+      toValue: { x: 0, y: 0}
+    }).start()
   }
 
   renderCards() {
