b4d1b9363073db3425322ac2bc0d19c5ae6eb12d
Weiming Zhai <wmzhai@gmail.com>
2017-04-13 15:22:29 +0800

    forceSwipe

--- src/deck.js
+++ src/deck.js
@@ -8,6 +8,7 @@ import {
 
 const SCREEN_WIDTH = Dimensions.get('window').width
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH
+const SWIPE_OUT_DURATION = 250
 
 class Deck extends Component {
   constructor(props) {
@@ -21,9 +22,9 @@ class Deck extends Component {
       },
       onPanResponderRelease: (event, gesture) => {
         if(gesture.dx > SWIPE_THRESHOLD) {
-          console.log('swipe right')
+          this.forceSwipe('right');
         } else if (gesture.dx < -SWIPE_THRESHOLD) {
-          console.log('swipe left')
+          this.forceSwipe('left');
         } else {
           this.resetPosition()
         }        
@@ -44,6 +45,15 @@ class Deck extends Component {
       ...position.getLayout(),
       transform: [{rotate}]
     }
+  }
+
+  forceSwipe(direction) {
+    const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+
+    Animated.timing(this.state.position,{
+      toValue: { x, y: 0},
+      duration: SWIPE_OUT_DURATION
+    }).start()
   }
 
   resetPosition() {
